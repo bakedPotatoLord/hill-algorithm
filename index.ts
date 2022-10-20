513bf81897b0346922d3a2b7e048984157298aa1
= <918780@dpsk12.net>
2022-10-19 20:58:17 -0600

    made constants static

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import{Point} from './Point.js'
 import{Climber} from './Climber.js'
 
-let points = [
+const points = [
   [0,0],[0,1],
   [1,0],[1,1],[1,2],[1,3],
   [2,0],[2,1],[2,2],
@@ -12,9 +12,9 @@ let points = [
 ]
 
 
-let pointArr = Array.from(points,(v)=>new Point(v[0],v[1]))
+const pointArr = Array.from(points,(v)=>new Point(v[0],v[1]))
 
-let climber = new Climber(1,1,pointArr)
+const climber = new Climber(1,1,pointArr)
 
 
 while(!climber.update()){}
